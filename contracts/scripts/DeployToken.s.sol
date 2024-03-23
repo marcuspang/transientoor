ca50f902572b2a33830e50a7843eb71e223de03f
Marcus Pang Yu Yang <marcuspangyuyang@gmail.com>
2024-03-23 19:43:00 +0800

    feat(contracts): add deploy pool script

--- contracts/scripts/DeployToken.s.sol
+++ contracts/scripts/DeployToken.s.sol
@@ -6,19 +6,16 @@ import {TransientToken} from "../src/TransientToken.sol";
 
 contract DeployTransientToken is Script {
     event NewContract(address newContract);
-  
-
 
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
         vm.startBroadcast(deployerPrivateKey);
-        TransientToken token0 = new TransientToken("TokenOne","TT1");
+        TransientToken token0 = new TransientToken("TokenOne", "TT1");
         emit NewContract(address(token0));
         TransientToken token1 = new TransientToken("TokenTwo", "TT2");
         emit NewContract(address(token1));
 
         vm.stopBroadcast();
     }
-
 }
