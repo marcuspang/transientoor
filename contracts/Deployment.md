e7166dda9939bce4995ef43b116e3c0325d05b33
zengzengzenghuy <tayzh2799@gmail.com>
2024-03-23 16:34:37 +0800

    contracts: Sepolia deployment

---
+++ contracts/Deployment.md
@@ -0,0 +1,4 @@
+# Sepolia
+
+1. TokenOne: [0x5a38BB9D84fEBf451c18282767BB11119B1CAD19](https://sepolia.etherscan.io/address/0x5a38bb9d84febf451c18282767bb11119b1cad19#code)
+2. TokenTwo: [0xfcd57f579733a96c97608d6c7ff3a93151f4cf0e](https://sepolia.etherscan.io/address/0xfcd57f579733a96c97608d6c7ff3a93151f4cf0e#code)
---
+++ contracts/script/DeployToken.s.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.25;
+
+import "forge-std/Script.sol";
+import {TransientToken} from "../src/TransientToken.sol";
+
+contract DeployTransientToken is Script {
+    event NewContract(address newContract);
+  
+
+
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        vm.startBroadcast(deployerPrivateKey);
+        TransientToken token0 = new TransientToken("TokenOne","TT1");
+        emit NewContract(address(token0));
+        TransientToken token1 = new TransientToken("TokenTwo", "TT2");
+        emit NewContract(address(token1));
+
+        vm.stopBroadcast();
+    }
+
+}
