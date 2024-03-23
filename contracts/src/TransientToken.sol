44cda3c75bdb912b004df7c7fa5d989b0448b030
zengzengzenghuy <tayzh2799@gmail.com>
2024-03-23 14:26:13 +0800

    test

---
+++ contracts/src/TransientToken.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.25;
+
+import {ERC20} from "./ERC20.sol";
+contract TransientToken is ERC20 {
+
+    uint256 public count;
+    event Count(uint256 count);
+    constructor(string memory name, string memory symbol)ERC20(name, symbol){}
+    
+    function transient() external {
+        bytes32 countSlot = bytes32(0);
+        assembly{
+            tstore(countSlot,30)
+        }
+        emit Count(count);
+
+    }
+
+    // function transferFrom(address from, address to, uint256 amount) external {
+    //     bytes memory allowanceSlot = keccak256(abi.encodePacked(from,keccak256(abi.encodePacked(to,uint256(1)))));
+    //     assembly{
+    //         tstore(slot,value)
+    //     }
+    //     super.transferFrom(from, to, amount);
+    //     assembly{
+    //         tstore(slot,0)
+    //     }
+    // }
+        
+}
