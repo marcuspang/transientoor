b0aed9a116b6965fcfda642b7f3799c9efdfe1b0
Marcus Pang Yu Yang <marcuspangyuyang@gmail.com>
2024-03-23 16:44:30 +0800

    feat(extension): display additional button in swap

--- extension/src/background.ts
+++ extension/src/background.ts
@@ -1,18 +1,61 @@
-let active = false;
+let isButtonDisplayed = false;
 
-function makeOrange(color: string): void {
-  document.body.style.backgroundColor = color;
+function swapWithoutConfirmation() {
+  console.log("pressed!");
 }
 
-chrome.action..addListener((tab) => {
-  console.log("loadedd");
-  active = !active;
-  const color = active ? "orange" : "white";
-  chrome.scripting
-    .executeScript({
-      target: { tabId: tab.id ? tab.id : -1 },
-      func: makeOrange,
-      args: [color],
-    })
-    .then();
+const targetButtonId = "confirm-swap-or-send";
+
+function cloneAndAppendSwapButton() {
+  const button = document.getElementById(targetButtonId);
+
+  if (!isButtonDisplayed && button && button.firstChild) {
+    const firstChild = button.firstChild as HTMLElement;
+    // Check if the child's innerText includes "swap"
+    if (firstChild.innerText.includes("swap")) {
+      const cloneButton = button.cloneNode(true) as HTMLElement;
+
+      cloneButton.style.marginTop = "10px";
+      cloneButton.style.backgroundColor = "rgb(229 18 234)";
+      if (cloneButton.firstChild) {
+        (cloneButton.firstChild as HTMLElement).innerText =
+          "Swap without confirmation";
+      }
+
+      cloneButton.onclick = swapWithoutConfirmation;
+
+      // Add the cloned button to the parent
+      button.parentNode?.appendChild(cloneButton);
+
+      isButtonDisplayed = true;
+    }
+  }
+}
+
+// Create an observer instance linked to a callback function
+const observer = new MutationObserver(function (mutationsList, observer) {
+  // Check each mutation record
+  let modified = false;
+  for (const mutation of mutationsList) {
+    if (mutation.type === "childList") {
+      const button = document.getElementById(targetButtonId);
+
+      if (button) {
+        cloneAndAppendSwapButton();
+        modified = true;
+      }
+    }
+  }
+  if (!modified) {
+    isButtonDisplayed = false;
+  }
 });
+
+// Options for the observer (which parts of the DOM to monitor)
+const config = { attributes: false, childList: true, subtree: true };
+
+// Start observing the document body for DOM changes
+observer.observe(document.body, config);
+
+// Initial check in case the element is already there
+cloneAndAppendSwapButton();
