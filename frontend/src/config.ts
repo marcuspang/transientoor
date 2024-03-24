9f31fd7c002863d68c058f2605bb27cb93afb87d
Marcus Pang Yu Yang <marcuspangyuyang@gmail.com>
2024-03-24 09:29:39 +0800

    feat(frontend): add optimism sepolia + toaster

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,11 +1,18 @@
 import { http } from "viem";
-import { mainnet, sepolia } from "viem/chains";
+import {
+  mainnet,
+  optimism,
+  optimismSepolia,
+  sepolia
+} from "viem/chains";
 import { createConfig } from "wagmi";
 
 export const config = createConfig({
-  chains: [mainnet, sepolia],
+  chains: [sepolia, optimismSepolia, mainnet, optimism],
   transports: {
     [mainnet.id]: http(),
     [sepolia.id]: http(),
+    [optimism.id]: http(),
+    [optimismSepolia.id]: http(),
   },
 });
